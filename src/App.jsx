d44e3feaa68e9d29885136625398965cb1990377
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-10-14 23:03:29 +0100

    calculadora-simples

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Calculadora from './components/Calculadora'
+
+function App() {
+
+  return (
+ <div>
+  <Calculadora/>
+ </div>
+  )
+}
+
+export default App
---
+++ src/components/Calculadora.jsx
@@ -0,0 +1,11 @@
+export default function Calculadora (){
+    return(
+        <div>
+            <h1>Calculadora</h1>
+            <p>Resultado</p>
+            <input/>
+            <input/>
+            <button>efetuar</button>
+        </div>
+    )
+}
