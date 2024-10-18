2c3a0dd5e1c29c8c32dcb114e84814ef1e86c145
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-10-18 18:53:08 +0100

    calculadora-simples

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,28 +1,31 @@
+import { useState } from "react"
+
 export default function Calculadora (){
+    const [input, setInput] = useState ("")
     return(
         <div className="container">
             <h1>Calculadora</h1>
-            <p>Resultado</p>
+            <p>{input}</p>
         <div>
-            <button>CE</button>
-            <button>C</button>
-            <button>DEL</button>
-            <button>/</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>*</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>-</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
+            <input type="button" value="CE" onClick={e => setInput("")}  />
+            <input type="button" value="C" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="DEL" onClick={e => setInput( input.slice(0, -1))} />
+            <input type="button" value="/" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="7" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="8" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="9" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="*" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="4" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="5" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="6" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="-" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="1" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="2" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="3" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="+" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="0" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="." onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="=" onClick={e => setInput( input + e.target.value)} />
         </div>
         </div>
     )
