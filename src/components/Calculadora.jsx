0276f1ed94f80391966f610275869bf7518eb0ec
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-10-18 21:21:51 +0100

    calculadora-simples

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -3,12 +3,15 @@ import { useState } from "react"
 export default function Calculadora (){
     const [input, setInput] = useState ("")
     return(
+        <>
+        <h1>Calculadora</h1>
         <div className="container">
-            <h1>Calculadora</h1>
+            <div className="tela">
             <p>{input}</p>
-        <div>
+            </div> 
+       <div>
             <input type="button" value="CE" onClick={e => setInput("")}  />
-            <input type="button" value="C" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="%" onClick={e => setInput( input + e.target.value)} />
             <input type="button" value="DEL" onClick={e => setInput( input.slice(0, -1))} />
             <input type="button" value="/" onClick={e => setInput( input + e.target.value)} />
             <input type="button" value="7" onClick={e => setInput( input + e.target.value)} />
@@ -25,8 +28,9 @@ export default function Calculadora (){
             <input type="button" value="+" onClick={e => setInput( input + e.target.value)} />
             <input type="button" value="0" onClick={e => setInput( input + e.target.value)} />
             <input type="button" value="." onClick={e => setInput( input + e.target.value)} />
-            <input type="button" value="=" onClick={e => setInput( input + e.target.value)} />
+            <input type="button" value="=" onClick={e => setInput(eval(input))} />
         </div>
         </div>
+        </>
     )
 }
